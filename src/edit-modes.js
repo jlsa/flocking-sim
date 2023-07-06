14a8b2429addda99507750a97dbf34b7b07f95bb
jlsa <jlsahoekstra@gmail.com>
2023-07-06 07:37:43 +0200

    Moved edit modes to its own file

---
+++ src/edit-modes.js
@@ -0,0 +1,26 @@
+const editModes = {
+    default: {
+        keyBind: 'd',
+        value: 'default',
+        radius: 50,
+        color: { r: 255, g: 215, b: 141, a: 5 }
+    },
+    spawn: {
+        keyBind: 's',
+        value: 'spawn',
+        radius: 300,
+        color: { r: 94, g: 140, b: 102, a: 10 }
+    },
+    erase: {
+        keyBind: 'e',
+        value: 'erase',
+        radius: 200,
+        color: { r: 140, g: 80, b: 94, a: 10 }
+    },
+    select: {
+        keyBind: 'v',
+        value: 'select',
+        radius: 25,
+        color: { r: 164, g: 179, b: 193, a: 10 }
+    },
+};
